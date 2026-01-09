7645a846016446872dec67155bbf4898ed548ab8
Artemchi-web <153541898+Artemchi-web@users.noreply.github.com>
2026-01-09 21:02:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -81,9 +81,10 @@ let tg = window.Telegram.WebApp;
 let order_but = document.getElementById("order-button")
 
 order_but.addEventListener("click", () => {
-  if (total > 0){
+  if (cart){
     tg.sendData(JSON.stringify(cart));
-  }else{
+  }
+  else{
     alert("В корзине ничего нету")
   }
 });
